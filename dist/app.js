a90baf5942f7204986ec233fb59cd48208a840de
eonduck2 <whdtnxd@gmail.com>
2024-07-02 10:51:12 +0900

    :package: 이종수: 간단한 서버 구현

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,21 @@
 "use strict";
-const a = (a, b) => {
-    const result = a + b;
-    return result;
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+http_1.default
+  .createServer((req, res) => {
+    if (req.method === `GET`) {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-type": "text/plain" });
+        res.end(`ㅎㅇ`);
+      }
+    }
+  })
+  .listen(3000, () => {
+    console.log(`https://localhost:3000`);
+  });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
-const a = (a: number, b: number): number => {
-  const result = a + b;
-  return result;
-};
+import http from "http";
+
+http
+  .createServer((req, res) => {
+    if (req.method === `GET`) {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-type": "text/plain" });
+        res.end(`ㅎㅇ`);
+      }
+    }
+  })
+  .listen(3000, () => {
+    console.log(`https://localhost:3000`);
+  });
